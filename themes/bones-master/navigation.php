e00de8782cd50f96f63bedc1f52cc2280e9e856f
Msoshiza <maz0lla93@gmail.com>
2018-04-30 02:28:10 -0400

    test push

--- themes/bones-master/navigation.php
+++ themes/bones-master/navigation.php
@@ -5,7 +5,7 @@
          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
          <span class="navbar-toggler-icon"></span>
          </button>
-         
+
          <form class="form-inline my-2 my-lg-0">
           <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
           <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
